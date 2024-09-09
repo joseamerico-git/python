c362718d24c0157a561a4296632840063c678576
Jose Americo <doichejunior@gmail.com>
2024-09-09 16:07:30 -0300

    operações básicas em python

---
+++ app.py
@@ -0,0 +1,50 @@
+# Imprimindo no console.
+print("Olá")
+# Declarando variáveis
+a = 5
+b = 9
+
+
+nome = "JOSE AMERICO"
+
+# Função com parametros + retorno
+def somar(a,b):
+    return (a+b)
+
+# format string
+print(f'A soma é : {somar(a,b)}, {nome}')
+
+
+# calculando um número elevado 
+
+# potênciação
+quadrado = a**2
+cubo = a**(3)
+
+
+print(f'O Quadrado de a é igual a: = {quadrado}.')
+print(f'O cubo de a é igual a: = {cubo}.')
+
+# Raiz Quadrada de 4
+
+raizDeQuatro = 4**(1/2)
+
+print(f'A raiz quadrada do número 4 é {raizDeQuatro}')
+
+# Mod
+
+# num =7;
+# num1 =3;
+
+num =4;
+num1 =2;
+
+print (f'Utilizando mod em phython {num} mod {num1} = {num % num1}')
+
+
+
+
+
+
+
+
