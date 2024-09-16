f4cb057319c9fccca1139eac45e06d2622d0572f
Jose Americo <doichejunior@gmail.com>
2024-09-16 09:30:41 -0300

    alteracoes em md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# python
+
+![](accets/python.jfif) 
+
+# Aprendendo Python
+
+ As declarações de variáveis em python, devem seguir, por convensão padrão, 
+ iniciando sempre com letras minúsculas, nunca iniciar com números.
+ Em casos de nomes compostos, utilizamos o simbolo '_'(underline) entre as palavras. 
+ Vejamos exemplos abaixo:
+
+[Variáveis em Python](variaveis.py)
---
+++ variaveis.py
@@ -0,0 +1,15 @@
+# As declarações de variáveis em python, devem seguir, por convensão um padrão, 
+# iniciando sempre com letras minúsculas e nunca iniciar com números.
+# Em casos de nomes compostos, utilizamos o '_' entre as palavras. 
+# Vejamos exemplos abaixo:
+
+# Declarando as variáveis:
+
+titulo = 'Aprendendo Python'
+nome_pessoa = 'José Américo'
+titulo1 ='do Zero'
+
+print(f'Título: {titulo} {titulo1}')
+print('Nome: '+ nome_pessoa)
+
+
